5b1929d76379cbfd79a2ba2f4265fcdee6891d2c
Sami Lunnamo <ext-sami.lunnamo@otava.fi>
2025-02-19 08:42:30 +0200

    Specify write and close as async operations for realine

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ import { stdin, stdout } from "node:process";
 
 const rl = readline.createInterface({ input: stdin, output: stdout });
 // Use `const answer = await rl.question('write something ')`
-// rl.write to write stuff to terminal
-// rl.close to cleanup
+// `await rl.write('stuff')` to write stuff to terminal
+// `await rl.close()` to cleanup
